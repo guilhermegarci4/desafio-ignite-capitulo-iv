623a3f523049243afa3fe428257c71aa892cd83a
Guilherme Garcia <guillherme966@gmail.com>
2021-05-13 20:58:41 -0300

    transfer

--- src/database/migrations/1616682561481-statements-table.ts
+++ src/database/migrations/1616682561481-statements-table.ts
@@ -3,55 +3,44 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class accountsTable1616682561481 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: 'statements',
-      columns: [
-        {
-          name: 'id',
-          type: 'uuid',
-          isPrimary: true,
-        },
-        {
-          name: 'user_id',
-          type: 'uuid',
-        },
-        {
-          name: 'description',
-          type: 'varchar',
-        },
-        {
-          name: 'amount',
-          type: 'decimal',
-          precision: 5,
-          scale: 2,
-        },
-        {
-          name: 'type',
-          type: 'enum',
-          enum: ['deposit', 'withdraw']
-        },
-        {
-          name: 'created_at',
-          type: 'timestamp',
-          default: 'now()'
-        },
-        {
-          name: 'updated_at',
-          type: 'timestamp',
-          default: 'now()'
-        }
-      ],
-      foreignKeys: [
-        {
-          name: 'statements',
-          columnNames: ['user_id'],
-          referencedTableName: 'users',
-          referencedColumnNames: ['id'],
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE'
-        }
-      ]
-    }))
+    await queryRunner.createTable(
+      new Table({
+        name: 'transfers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'sender_id',
+            type: 'uuid'
+          },
+          {
+            name: 'receiver_id',
+            type: 'uuid'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FK_sender_id_transfers',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['sender_id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+          },
+          {
+            name: 'FK_recipient_id_transfers',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['receiver_id'],
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+          }
+        ]
+      })
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
